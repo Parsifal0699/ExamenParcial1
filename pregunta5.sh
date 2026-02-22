685534fd0b8ac49994c50333bfac7020fec35414
Juan Pablo Ruiz Sandoval <parsifal065@gmail.com>
2026-02-21 19:19:03 -0600

    Agrego pregunta5.sh

---
+++ pregunta5.sh
@@ -0,0 +1,21 @@
+1  sudo apt update
+    2  sudo apt upgrade
+    3  vim
+    4  sudo apt install vim
+    5  vim
+    6  clear
+    7  ls
+    8  sudo apt install cmake
+    9  python
+   10  sudo apt install vscode
+   11  cd Downloads/
+   12  ls
+   13  sudo apt install code_1.108.1-1768404234_amd64.deb
+   14  sudo apt install ./code_1.108.1-1768404234_amd64.deb 
+   15  cd ~ 
+   16  ls -la
+   17  pwd
+   18  top
+   19  touch accept.txt
+   20  touch accept.txt ; ls > accept.txt; history | grep -x "ls" | wc -l >> accept.txt
+   21  history
